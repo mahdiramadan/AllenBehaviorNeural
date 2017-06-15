0523341cdf196d0844bbc5eb5b3e84a4e1d5e822
Mahdi Ramadan <mahdir@alleninstitute.org>
2017-06-15 12:29:11 -0700

    script for generating AUC curves

--- Scripts/DATA_ANALYSIS.py
+++ Scripts/DATA_ANALYSIS.py
@@ -18,8 +18,10 @@ from skimage.feature import hog
 from skimage import data, color, exposure
 from scipy.optimize import curve_fit
 from sklearn.cross_validation import train_test_split
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.grid_search import GridSearchCV
 from sklearn.metrics import classification_report
+from sklearn import neighbors, datasets
 from sklearn.svm import SVC
 from sklearn import preprocessing
 from multiprocessing import Process
@@ -36,7 +38,13 @@ import time
 import pdb
 from psycopg2 import connect
 import warnings
-
+from datetime import datetime
+from sklearn import svm, datasets
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
+from scipy import interp
 
 
 
@@ -80,267 +88,158 @@ def run (ids , ann_data):
                'natural_movie_three_stimulus', 'static_gratings_stimulus',
                'locally_sparse_noise_stimulus']
 
-    results_fid = [[]for _ in range(len(stimuli))]
-    results_mov = [[] for _ in range(len(stimuli))]
-    results_neither = [[] for _ in range(len(stimuli))]
-
-    func = 0
-
     A_count = 0
     B_count = 0
     C_count = 0
 
-
-    stim_A_fidget_cux2 = []
-    stim_A_fidget_emx1 = []
-    stim_A_fidget_nr5a1 = []
-    stim_A_fidget_rbp4 = []
-    stim_A_fidget_rorb = []
-    stim_A_fidget_scnn1a = []
-
-    stim_B_fidget_cux2 = []
-    stim_B_fidget_emx1 = []
-    stim_B_fidget_nr5a1 = []
-    stim_B_fidget_rbp4 = []
-    stim_B_fidget_rorb = []
-    stim_B_fidget_scnn1a = []
-
-    stim_A_fidget = []
-    stim_B_fidget = []
-    stim_C_fidget = []
-
-    excel_data = pandas.read_excel('C:\Users\mahdir\Documents\Allen Projects\Behavior Annotation\global_report.xlsx', sheetname = 'all')
-    excel_ids = excel_data['lims ID']
-    a= excel_ids[0]
-
+    # excel_data = pandas.read_excel('C:\Users\mahdir\Documents\Allen Projects\Behavior Annotation\global_report.xlsx', sheetname = 'all')
+    # excel_ids = excel_data['lims ID']
+    # rig_list = excel_data['lims_database_operator']
+    # unique = np.unique(rig_list)
+    #
+    #
+    #
+    # # unique_operator = np.unique(operator_list).tolist()
+    # rig_results = [[] for _ in range(len(unique))]
 
     for id in ids:
-        try :
-            qc_status = np.array(find_status(id))
-            mouse = qc_status[(0)][0].split(';')
-            cre = mouse[0].split(',')
-            creline = cre[0]
-            mouse_id_whole = mouse[2].split('-')
-            mouse_id = mouse_id_whole[1]
+        qc_status = np.array(find_status(id))
+        stim = qc_status[(0)][3]
 
+        if stim == 'three_session_A':
+            A_count += 1
 
-            stim = qc_status[(0)][3]
-            count_t = 0
+    A_anxiety = [[] for _ in range(A_count*2)]
 
-            if stim == 'three_session_B':
-                data = ann_data[count]
+    A_count = 0
+    y_train=[]
 
-                t_fid = 0
-                for f in range(len(data)):
-                    if data[f] == 0:
-                        t_fid += 1
+    for id in ids:
 
-                for m in ids:
+        data = ann_data[count]
+        qc_status = np.array(find_status(id))
+        stim = qc_status[(0)][3]
+
+        if stim == 'three_session_A':
+
+            t_fid = 0
+            for f in range(len(data)):
+                if data[f] == 0:
+                    t_fid += 1
+
+            t_neither = 0
+            for f in range(len(data)):
+                if data[f] == 1:
+                    t_neither += 1
+
+            t_mov = 0
+            for f in range(len(data)):
+                if data[f] == 2:
+                    t_mov += 1
+
+            base = (float(t_fid) / len(data)) * 100
+
+            block = []
+            for i in range (0, len(data), 1000):
+                p = 0
+                for k in range (i, i+1000):
                     try:
-                        qc_status_t = np.array(find_status(m))
-                        mouse_t = qc_status_t[(0)][0].split(';')
-                        cre_t = mouse_t[0].split(',')
-                        creline_t = cre_t[0]
-                        mouse_id_whole_t= mouse_t[2].split('-')
-                        mouse_id_t= mouse_id_whole_t[1]
-                        stim_t = qc_status_t[(0)][3]
-
-                        if mouse_id == mouse_id_t and stim_t == 'three_session_C' and m != id:
-                            data_t = ann_data[count_t]
-
-                            t_fid_t = 0
-                            for f in range(len(data_t)):
-                                if data_t[f] == 0:
-                                    t_fid_t += 1
-                            if creline_t == 'Cux2-CreERT2':
-                                stim_A_fidget_cux2.append(round((float(t_fid)/len(data))*100, 2))
-                                stim_B_fidget_cux2.append(round ((float(t_fid_t)/len(data_t))*100, 2))
-                            elif creline_t == 'Emx1-IRES-Cre':
-                                stim_A_fidget_emx1.append(round((float(t_fid)/len(data))*100, 2))
-                                stim_B_fidget_emx1.append(round ((float(t_fid_t)/len(data_t))*100, 2))
-                            elif creline_t == 'Nr5a1-Cre':
-                                stim_A_fidget_nr5a1.append(round((float(t_fid)/len(data))*100, 2))
-                                stim_B_fidget_nr5a1.append(round ((float(t_fid_t)/len(data_t))*100, 2))
-                            elif creline_t == 'Rbp4-Cre':
-                                stim_A_fidget_rbp4.append(round((float(t_fid)/len(data))*100, 2))
-                                stim_B_fidget_rbp4.append(round ((float(t_fid_t)/len(data_t))*100, 2))
-                            elif creline_t == 'Rorb-IRES2-Cre':
-                                stim_A_fidget_rorb.append(round((float(t_fid)/len(data))*100, 2))
-                                stim_B_fidget_rorb.append(round ((float(t_fid_t)/len(data_t))*100, 2))
-                            elif creline_t == 'Scnn1a-Tg3-Cre':
-                                stim_A_fidget_scnn1a.append(round((float(t_fid)/len(data))*100, 2))
-                                stim_B_fidget_scnn1a.append(round ((float(t_fid_t)/len(data_t))*100, 2))
-                            else:
-                                print(creline_t)
-
-
-                        count_t += 1
+                        if data[k] == 0:
+                            p += 1
                     except:
-                        count_t += 1
                         continue
+                block.append(round((float(p/float(1000))*100)/base, 2))
 
-            count += 1
-        except:
-            count +=1
-            continue
+            data = block
 
+            A_anxiety[A_count] = np.hstack((data[40:50], data[72:82]))
+            A_anxiety[(A_count +1)] = np.hstack((data[20:40]))
+            y_train.append(0)
+            y_train.append(1)
+            A_count += 2
+            # for index, itm in enumerate(excel_ids):
+            #     if str(itm).strip() == id.strip():
+            #         track = index
+            #
+            #
+            #
+            # if track is not empty:
+            #     info = str(rig_list[track]).strip().split('.')
+            #     rig = info[1]
+            #     rig_results[int(rig)].append(round((float(t_fid)/len(data))*100, 2))
 
-
-    all_A = np.concatenate((stim_A_fidget_cux2, stim_A_fidget_emx1, stim_A_fidget_nr5a1, stim_A_fidget_rbp4, stim_A_fidget_rorb, stim_A_fidget_scnn1a))
-    all_B = np.concatenate((stim_B_fidget_cux2, stim_B_fidget_emx1, stim_B_fidget_nr5a1, stim_B_fidget_rbp4, stim_B_fidget_rorb, stim_B_fidget_scnn1a))
-
-    plt.title(' Intra-Mouse Stim B versus stim C Fidget rate')
-    plt.ylabel('Stimulus C Fidget Rate')
-    plt.xlabel('Stimulus B Fidget Rate')
-    plt.show(hist2d(all_A, all_B, bins= 18))
-
-    # colors = ['b', 'c', 'y', 'm', 'r', 'k']
-    #
-    # cux2 = plt.scatter(stim_A_fidget_cux2, stim_B_fidget_cux2, marker='o', color=colors[0])
-    # emx1 = plt.scatter(stim_A_fidget_emx1, stim_B_fidget_emx1, marker='o', color=colors[1])
-    # nr5a1 = plt.scatter(stim_A_fidget_nr5a1, stim_B_fidget_nr5a1, marker='o', color=colors[2])
-    # rbp4 = plt.scatter(stim_A_fidget_rbp4, stim_B_fidget_rbp4, marker='o', color=colors[3])
-    # rorb = plt.scatter(stim_A_fidget_rorb, stim_B_fidget_rorb, marker='o', color=colors[4])
-    # scnn1a = plt.scatter(stim_A_fidget_scnn1a, stim_B_fidget_scnn1a, marker='o', color=colors[5])
-    #
-    # plt.legend((cux2, emx1, nr5a1, rbp4, rorb, scnn1a),
-    #            ('Cux2', 'Emx1', 'Nr5a1', 'Rbp4', 'Rorb', 'Scnn1a'),
-    #            scatterpoints=1,
-    #            loc='lower right',
-    #            ncol=3,
-    #            fontsize=8)
-    #
-    # plt.title(' Intra-Mouse Stim B versus stim C Fidget rate')
-    # plt.ylabel('Stimulus B Fidget Rate')
-    # plt.xlabel('Stimulus C Fidget Rate')
-    # plt.show()
-
-    # workbook = xlsxwriter.Workbook('C:\Users\mahdir\Documents\Allen Projects\Behavior Annotation\\A_cre.xlsx')
-    # worksheet = workbook.add_worksheet()
-    #
-    #
-    # worksheet.write_row(11, 0, stim_A_fidget_scnn1a)
-    # # worksheet.write_row(12, 0, stim_B_fidget_scnn1a)
-    # workbook.close()
+        count += 1
 
 
-    #     if stim == 'three_session_A':
-    #         A_count += 1
-    #     if stim == 'three_session_B':
-    #         B_count += 1
-    #     if stim == 'three_session_C':
-    #         C_count += 1
-    #
-    #
-    # time_A = [[] for _ in range(A_count)]
-    # time_B = [[] for _ in range(B_count)]
-    # time_C = [[] for _ in range(C_count)]
-    #
-    # A_count = 0
-    # B_count = 0
-    # C_count = 0
 
-    # for id in ids:
-    #
-    #     qc_status = np.array(find_status(id))
-    #     status = qc_status[(0)][2]
-    #
-    #     # if status is not None and 'failed' not in status:
-    #
-    #
-    #     func += 1
-    #     stimulus = qc_status[(0)][3]
-    #     data = ann_data[count]
-    #     count += 1
-    #
-    #
-    #     t_fid = 0
-    #     for f in range(len(data)):
-    #         if data[f] == 0:
-    #             t_fid += 1
-    #
-    #     t_neither = 0
-    #     for f in range(len(data)):
-    #         if data[f] == 1:
-    #             t_neither += 1
-    #
-    #     t_mov = 0
-    #     for f in range(len(data)):
-    #         if data[f] == 2:
-    #             t_mov += 1
-    #
-    #     base = (float(t_fid)/len(data))*100
-    #
-    #     block = []
-    #     for i in range (0, len(data), 1000):
-    #         p = 0
-    #         for k in range (i, i+1000):
-    #             try:
-    #                 if data[k] == 0:
-    #                     p += 1
-    #             except:
-    #                 continue
-    #         block.append(round((float(p/float(1000))*100)/base, 2))
-    #
-    #     data = block
-    #
-    #
-    #     if stimulus == 'three_session_A':
-    #
-    #         time_A[A_count] = data
-    #         A_count += 1
-    #
-    #     if stimulus == 'three_session_B':
-    #
-    #         time_B[B_count] = data
-    #         B_count += 1
-    #
-    #     if stimulus == 'three_session_C':
-    #         time_C[C_count] = data
-    #         C_count += 1
-    #
-    #
-    # # print np.apply_along_axis(myfunction, axis=1, arr=time_A)
-    # # print np.apply_along_axis(myfunction, axis=1, arr=time_B)
-    # # print np.apply_along_axis(myfunction, axis=1, arr=time_C)
-    #
-    #         # Create a new workbook and add a worksheet
-    #
-    #
-    # workbook = xlsxwriter.Workbook('C:\Users\mahdir\Documents\Allen Projects\Behavior Annotation\\.xlsx')
-    # worksheet = workbook.add_worksheet()
-    #
-    # for row, data in enumerate(time_C):
-    #     worksheet.write_row(row, 0, data)
-    # workbook.close()
-    #
+        # workbook = xlsxwriter.Workbook('C:\Users\mahdir\Documents\Allen Projects\Behavior Annotation\\rig_results.xlsx')
+        # worksheet = workbook.add_worksheet()
+        #
+        # for index, row in enumerate(rig_results):
+        #     worksheet.write_row(index, 0, row)
+        #
+        #
+        # workbook.close()
 
-def myplot(x, y, nb=32, xsize=500, ysize=500):
-    xmin = np.min(x)
-    xmax = np.max(x)
-    ymin = np.min(y)
-    ymax = np.max(y)
 
-    x0 = (xmin+xmax)/2.
-    y0 = (ymin+ymax)/2.
 
-    pos = np.zeros([3, len(x)])
-    pos[0,:] = x
-    pos[1,:] = y
-    w = np.ones(len(x))
+    x_data = A_anxiety[:]
+    y_data = y_train
 
-    P = sph.Particles(pos, w, nb=nb)
-    S = sph.Scene(P)
-    S.update_camera(r='infinity', x=x0, y=y0, z=0,
-                    xsize=xsize, ysize=ysize)
-    R = sph.Render(S)
-    R.set_logscale()
-    img = R.get_image()
-    extent = R.get_extent()
-    for i, j in zip(xrange(4), [x0,x0,y0,y0]):
-        extent[i] += j
-    print extent
-    return img, extent
+    # rows_n = len(x_data)
+    # train = int(round(rows_n * 0.6))
+    # end = int(round(rows_n * 1.0))
+    #
+    # X_train = x_data[0:train]
+    # X_test = x_data[train:end]
+    #
+    # y_train = y_data[0:train]
+    # y_test = y_data[train:end]
+
+    X_train, X_test, y_train, y_test = train_test_split(x_data, y_data, test_size=.35,
+                                                        random_state=1)
+
+    # Learn to predict each class against the other
+    classifier = OneVsRestClassifier(svm.SVC(kernel='rbf', probability= True))
+    y_score = classifier.fit(X_train, y_train).decision_function(X_test)
+
+    # Compute ROC curve and ROC area for each class
+    fpr = dict()
+    tpr = dict()
+    roc_auc = dict()
+
+    fpr[0], tpr[0], _ = roc_curve(y_test[:], y_score[:])
+    roc_auc[0] = auc(fpr[0], tpr[0])
+
+    # Compute micro-average ROC curve and ROC area
+    fpr["micro"], tpr["micro"], _ = roc_curve(y_test, y_score)
+    roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+    plt.figure()
+    lw = 2
+    plt.plot(fpr[0], tpr[0], color='darkorange',
+             lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[0])
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Natural Movie Three Stimulus Prediction from Fidget Rate by RBF SVM')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+
+    target_names = ['Other', 'Natural Movie Three']
+    y_true, y_pred = y_test, classifier.predict(X_test)
+    print(classification_report(y_true, y_pred, target_names=target_names))
+
+
+
+
+
+
+
 
 def myfunction (x):
     return len (x)
